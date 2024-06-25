c920550a870564156754c76a4fbc549fe41217a7
hieupth <64821726+hieupth@users.noreply.github.com>
2024-06-25 21:14:38 +0700

    chore: screen 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 
 import './App.css';
-import Image from 'react';
-import React from "react";
-import { Container, Row } from 'react-bootstrap';
+import React, { useCallback, useRef, useState } from "react";
+import { Container, Row, Col } from 'react-bootstrap';
 import Webcam from "react-webcam";
 
 function UserImage(props) {
+  const webcamRef = useRef(null);
+  const [imgSrc, setImgSrc] = useState(null);
+  const [imgWebSrc, setImgWebSrc] = useState(null);
+
+  function uploadFile(e) {
+    setImgSrc(URL.createObjectURL(e.target.files[0]));
+  }
+
+  // create a capture function
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  }, [webcamRef]);
+
+  function resetImage() {
+    setImgSrc(null);
+  }
+
   return (
-    <div className='userimage'></div>
+    <Row>
+      {imgSrc ? 
+      (
+        <Row>
+          <img src={imgSrc} alt="webcam" />
+          <button onClick={resetImage}>XÓA</button>
+          <button>HOÀN THÀNH</button>
+        </Row>
+      ) : (
+        <>
+        {/* <img src={imgSrc} alt="abc" /> */}
+        <div class='row'>
+          <Webcam height={600} width={600} ref={webcamRef} />
+          <button class="btn-upload-image" onClick={capture}>Chụp ảnh chân dung</button>
+        </div>
+        <div class='row'>
+          <label for="upload-image" class="btn-upload-image">Chọn từ máy tính</label>
+          <input id="upload-image" type="file" onChange={uploadFile}/>
+        </div>
+        </>
+      )}
+    </Row>
   );
 }
 
@@ -15,7 +53,10 @@ function App() {
   return (
     <Container>
       <Row>
-        <img class="slogan" src={process.env.PUBLIC_URL + 'slogan.png'}></img>
+        <img class="slogan center" src={process.env.PUBLIC_URL + 'slogan.png'}></img>
+      </Row>
+      <Row>
+        <UserImage></UserImage>
       </Row>
     </Container>
   );
